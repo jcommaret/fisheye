434c97be899abbfa35fcfa86037836882c23602c
jcommaret@gmail.com <jcommaret@gmail.com>
2022-08-18 11:31:41 +0200

    Mise a jour

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,13 +1,14 @@
 import { getData } from "../fetch_api"
 import { photographerFactory } from "../../js/factories/photographer";
 
+
 export async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
     const photographers = await getData();
         
     // et bien retourner le tableau photographers seulement une fois
     return ({
-        photographers: [...photographers]})
+        photographers: [photographers]})
 }
 
 export async function displayData(photographers) {

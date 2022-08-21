54c586ea0dd74dc37dbd9020fb9c68d1446aad42
jcommaret@gmail.com <jcommaret@gmail.com>
2022-08-21 20:42:36 +0200

    Component : modale

---
+++ js/utils/modal.js
@@ -0,0 +1,17 @@
+// modal button
+const modalBtn = document.querySelectorAll(".modal-toogle");
+// modal window
+const modal = document.querySelector(".modal");
+// launch modal function on every click on Modal btn
+modalBtn.forEach((btn) => btn.addEventListener("click", Modal));
+
+// Modal function : check if modal has class open and add it if not, remove it if this is the case 
+function Modal() {
+  if (!modal.classList.contains("open")){
+    modal.classList.remove("hide");
+    modal.classList.add("open");
+    }
+    else{
+      modal.classList.replace("open","hide");
+    }
+}

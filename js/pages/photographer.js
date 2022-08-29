ce212e93afecd64b61bdd943fe835ea1175668c2
jcommaret@gmail.com <jcommaret@gmail.com>
2022-08-29 17:27:00 +0200

    Hourra : Lightbox is displaying medias

--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -10,14 +10,19 @@ import "../utils/form"
 import { getMediaById } from "../fetch_api";
 import { MediaFactory } from "../factories/media";
 
+// working
 export async function displayMedias(media) {
   const mediasSection = document.querySelector(".medias_section");
+  const lightboxSection = document.querySelector(".lightbox_section");
   media.forEach((m) => {
     const type = m.image? "image" : "video" ;   
     const mediaModel = new MediaFactory(m, type);
-    mediasSection.innerHTML=mediaModel.render();
+    mediasSection.innerHTML+=mediaModel.render();
+    lightboxSection.innerHTML+=mediaModel.render();
   });
 };
+
+
 
 const init = async () => {
   const id = new URL(document.location).searchParams.get("id");

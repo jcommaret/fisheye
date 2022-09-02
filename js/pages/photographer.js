bfd7c0ecdf8e51795824c384517090cfff8afee8
jcommaret@gmail.com <jcommaret@gmail.com>
2022-09-02 16:29:16 +0200

    Modale

--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -3,26 +3,24 @@ import '../../sass/main.scss';
 
 // Import modale et lightbox
 import { Modal } from '../utils/modal';
-import { LightBox } from '../utils/lightbox';
+// import { LightBox } from '../utils/lightbox';
 
 import "../utils/form"
 // import JS functions
 import { getMediaById } from "../fetch_api";
 import { MediaFactory } from "../factories/media";
 
-// working
+// Working
 export async function displayMedias(media) {
   const mediasSection = document.querySelector(".medias_section");
-  const lightboxSection = document.querySelector(".lightbox_section");
+  // const lightboxSection = document.querySelector(".lightbox_section");
   media.forEach((m) => {
     const type = m.image? "image" : "video" ;   
     const mediaModel = new MediaFactory(m, type);
     mediasSection.innerHTML+=mediaModel.render();
-    lightboxSection.innerHTML+=mediaModel.render();
+    // lightboxSection.innerHTML+=mediaModel.render();
   });
 };
-
-
 
 const init = async () => {
   const id = new URL(document.location).searchParams.get("id");

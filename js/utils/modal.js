bd25a61f438bb96ae7b2ff48c94edbf4e405b4ca
jcommaret@gmail.com <jcommaret@gmail.com>
2022-09-05 19:06:55 +0200

    JS : Photographer

--- js/utils/modal.js
+++ js/utils/modal.js
@@ -1,31 +1,43 @@
-////////////// START MODALE /////////////
 
-// modal button
-const modalBtn = document.querySelectorAll(".modal-toogle");
-// modal window
-const modal = document.querySelector(".modal");
-// launch modal function on every click on Modal btn
-modalBtn.forEach((btn) => btn.addEventListener("click", Modal));
-
-// Modal function : check if modal has class open and add it if not, remove it if this is the case 
-export function Modal() {
-    if (!modal.classList.contains("open")){
-      modal.classList.remove("hide");
-      modal.classList.add("open");
-    }
-    else{
-      modal.classList.replace("open","hide");
-    }
-    
-    // Close modal if keyup on escape
-    document.addEventListener('keyup', function(event){
-	  if(event.key === "Escape"){
-		  modal.className = "modal hide";
-      setTimeout(()=>{
-        modal.style.display = 'none';
-      },200)
-	  }
-  });
+// Global DOM var
+const $body = document.getElementById('#photographer');
+const $openModalBtn = document.getElementsByClassName('.modal-toogle');
+const $mainWrapper = document.getElementsByClassName('.main-wrapper');
+const $modal = document.getElementsByClassName('.modal');
+const $modalTitle = document.getElementsByClassName('.modal-title');
+const $modalCloseBtn = document.getElementsByClassName('.modal-close-btn');
+ 
+// Func
+const onOpenModal = () => {
+   $mainWrapper.attr('aria-hidden', 'true');
+   $modal.attr('aria-hidden', 'false');
+   $body.addClass('no-scroll');
+   $modal.css('display', 'flex');
+   $modalCloseBtn.focus();
 }
-
-////////////// END MODALE /////////////
+ 
+const onCloseModal = () => {
+   $mainWrapper.attr('aria-hidden', 'false');
+   $modal.attr('aria-hidden', 'true');
+   $body.removeClass('no-scroll');
+   $modal.css('display', 'none');
+   $openModalBtn.focus();
+}
+ 
+// Event
+//$openModalBtn.addEventListener.click(function() {
+  // onOpenModal()
+//})
+ 
+// $modalCloseBtn.addEventListener.click(function() {
+ //  onCloseModal()
+//})
+ 
+// Close modal when espace key is pressed
+// $(document).on('keydown', e => {
+ //  const keyCode = e.keyCode ? e.keyCode : e.which
+ 
+//   if ($modal.attr('aria-hidden') == 'false' && keyCode === 27) {
+//       onCloseModal()
+//   }
+// })

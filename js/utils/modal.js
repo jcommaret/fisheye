dcb61bc3df47dd37ee010adfcf12d5059fe22698
jcommaret@gmail.com <jcommaret@gmail.com>
2022-09-07 16:22:30 +0200

    Working ?

--- js/utils/modal.js
+++ js/utils/modal.js
@@ -1,37 +1,10 @@
-// Global DOM var
-const $body = document.getElementById('#photographer');
-const $openModalBtn = document.getElementsByClassName('.modal-toogle');
-const $mainWrapper = document.getElementsByClassName('.main-wrapper');
-const $modal = document.getElementsByClassName('.modal');
-const $modalTitle = document.getElementsByClassName('.modal-title');
-const $modalCloseBtn = document.getElementsByClassName('.modal-close-btn');
- 
-// Func
-const onOpenModal = () => {
-   $mainWrapper.attr('aria-hidden', 'true');
-   $modal.attr('aria-hidden', 'false');
-   $body.addClass('no-scroll');
-   $modal.css('display', 'flex');
-   $modalCloseBtn.focus();
-}
- 
-const onCloseModal = () => {
-   $mainWrapper.attr('aria-hidden', 'false');
-   $modal.attr('aria-hidden', 'true');
-   $body.removeClass('no-scroll');
-   $modal.css('display', 'none');
-   $openModalBtn.focus();
-}
-
-////////////// START MODALE /////////////
-
-
-
 // modal button
 const modalBtn = document.querySelectorAll(".modal-toogle");
 // modal window
 const modal = document.querySelector(".modal");
 const fields = Array.of(modal.querySelectorAll(".fields"));
+
+console.log(fields);
 
 // launch modal function on every click on Modal btn
 modalBtn.forEach((btn) => btn.addEventListener("click", Modal));

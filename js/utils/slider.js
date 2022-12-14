dd14c6ded0592e0ec99c2615aceceff78ba44566
jcommaret@gmail.com <jcommaret@gmail.com>
2022-12-14 18:53:10 +0100

    Slider js

--- js/utils/slider.js
+++ js/utils/slider.js
@@ -6,28 +6,25 @@ export function ChangeSlide(sens) {
   numero = numero + sens;
   if (numero < 0)
       numero = slide.length - 1;
-  
   if (numero > slide.length - 1)
       numero = 0;
   const media = slide[numero];
-  console.log ("--- ", media.tagName);
   const node_= buildMedia(media.tagName, media.src);
   document.getElementById("slide").innerHTML = node_;
 }
 
 function buildMedia(tagName, source) {
   if (tagName.toLowerCase() === "img"){
-    return `<img src="${source}" alt=""/>`;
+    return `<img src="${source}" alt=""/> `;
   }else if (tagName.toLowerCase() === "video"){
     return `<video src="${source}" controls></video>`;
   }
 }
 
-
 export function init() {
   medias =  Array.from(document.querySelectorAll(".media-media"));
   const next = document.querySelector(".next");
-    const previous = document.querySelector(".previous");
-    next.addEventListener("click", () => ChangeSlide(1));
-    previous.addEventListener("click", () => ChangeSlide(-1));
+  const previous = document.querySelector(".previous");
+  next.addEventListener("click", () => ChangeSlide(1));
+  previous.addEventListener("click", () => ChangeSlide(-1));
 }
